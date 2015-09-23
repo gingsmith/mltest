8883ce9dd22e52ce8f9f8e35b3ccc26d584c6fcd
Ginger Smith <vsmith@berkeley.edu>
2015-09-22 18:09:45 -0700

    removing files

--- src/main/scala/optimize/Projecting.scala
+++
@@ -1,8 +0,0 @@
-package breeze.optimize
-
-import breeze.math.{Module}
-
-trait Projecting[T] {
-  def projection: T => T
-  def projectedVector(x: T, g: T)(implicit vspace: Module[T, Double]): T = vspace.subVV(projection(vspace.addVV(x, g)), x)
-}
